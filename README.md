fa6efe735ee32bee4c67ff7dd286e74ebe64f277
Charles HL <charles.hildle@gmail.com>
2023-05-14 16:09:47 +0200

    Creation of the script

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Email-Sender-Gmail
+# Email sender to multiple recipients with Gmail API
+
+This script allows you to send a single email to multiple recipients using the Gmail API. It reads the necessary information from text files and verifies the validity of the email addresses and content before sending the email.
+
+## Prerequisites
+Before using this script, make sure you have the following:
+- Google API credentials: You need to have a `credentials.json` file obtained by creating a project in the Google Developers Console and enabling the Gmail API. Place this file in the config directory. You can follow this guide to help you : [developers.google.com/gmail/api/quickstart/python](https://developers.google.com/gmail/api/quickstart/python)
+- Token file: A token.json file will be generated after authenticating the script with your Google account. If the file does not exist, it will be created automatically.
+- Text files with email information: The script reads the sender's email address, recipient email addresses, subject, and message from separate text files located in the email-info directory.
+
+## Installation
+1. Clone the repository and navigate to the project directory.
+2. Install the required dependencies by running the following command:
+```
+pip install requirements.txt
+```
+3. Ensure the following directory structure:
+```
+project/
+├── config/
+│   ├── credentials.json
+└── email-info/
+    ├── email_sender.txt
+    ├── email_recipients.txt
+    ├── email_subject.txt
+    └── email_message.txt
+```
+
+4. Replace the content of email_sender.txt with the sender's email address.
+
+5. Add recipient email addresses, one per line, in the `email_recipients.txt` file.
+
+6. Customize the email subject in the `email_subject.txt` file.
+
+7. Write the email message in the `email_message.txt` file.
+
+## Usage
+Run the script using the following command:
+```
+python main.py
+```
+The script will perform the following steps:
+
+- Check the credentials for the Gmail API and authenticate if necessary.
+
+- Read the sender's email address from email_sender.txt and validate its format.
+
+- Read the recipient email addresses from email_recipients.txt, validate their formats, and ensure there is at least one recipient.
+
+- Read the email subject from email_subject.txt, check its length (must not exceed 78 characters), and ensure it is not empty.
+
+- Read the email message from email_message.txt and ensure it is not empty.
+
+- Send the email to each recipient sequentially, with a delay of TIME_BETWEEN_EMAILS_IN_S seconds between each email.
+
+The script will output status messages to the console, indicating the progress of the email sending process.
+
+Note: The script assumes that the Gmail API is enabled and the necessary permissions are granted to the credentials associated with the credentials.json file.
+
---
+++ main.py
@@ -0,0 +1,118 @@
+"""
+Copyright (c) 2023, Charles HL. All rights reserved.
+"""
+import os
+import base64
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.auth.transport.requests import Request
+import google_auth_oauthlib.flow
+import re
+import email.utils as email_utils
+import email.message as email_message_lib
+import time
+
+TIME_BETWEEN_EMAILS_IN_S = 4 # in seconds
+
+def is_valid_email(email):
+    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    if re.match(pattern, email) or email_utils.parseaddr(email)[1]:
+        return True
+    else:
+        return False
+
+def create_message(sender, to, subject, message_text):
+    message = email_message_lib.EmailMessage()
+    message['From'] = sender
+    message['To'] = to
+    message['Subject'] = subject
+    message.set_content(message_text, subtype='plain', charset='utf-8')
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode('utf-8')}
+
+def send_message(message):
+    try:
+        service.users().messages().send(userId='me', body=message).execute()
+        print("Email sent successfully.")
+    except HttpError as error:
+        print(f"An error occurred while sending the email: {error}")
+
+def send_emails(sender, recipients, subject, message_text):
+    for recipient in recipients:
+        message = create_message(sender, recipient, subject, message_text)
+        print(f"Sending email to {recipient}...")
+        send_message(message)
+        if recipient != recipients[-1]:
+            print(f"Waiting {TIME_BETWEEN_EMAILS_IN_S} seconds before sending the next email...")
+            time.sleep(TIME_BETWEEN_EMAILS_IN_S)
+
+if __name__ == '__main__':
+
+    print("-----------------------------")
+    print("Script to send one email to multiple email addresses with Gmail API")
+    print("-----------------------------")
+
+    print("\nChecking credentials for Gmail API...")
+    # Set up credentials
+    credentials = None
+    if os.path.exists('config/token.json'):
+        credentials = Credentials.from_authorized_user_file('config/token.json')
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                'config/credentials.json', ['https://www.googleapis.com/auth/gmail.send'])
+            credentials = flow.run_local_server(port=0)
+        with open('config/token.json', 'w') as token:
+            token.write(credentials.to_json())
+
+    # Create a Gmail API service
+    service = build('gmail', 'v1', credentials=credentials)
+    
+    # Read email address of the sender from a text file
+    with open('email-info/email_sender.txt', 'r') as file:
+        sender_email = file.read()
+    # Check if the sender email is valid
+    if not is_valid_email(sender_email):
+        raise Exception(f"Invalid email: {sender_email}")
+    # Check if the file only contains one email address
+    if '\n' in sender_email or '\r' in sender_email:
+        raise Exception("Sender email contains line breaks.")    
+
+    print("Sender email: " + sender_email)
+
+    print("Checking email recipients, subject and message...")
+
+    # Read recipient emails from a text file where each line is an email address
+    with open('email-info/email_recipients.txt', 'r') as file:
+        recipient_emails = file.readlines()
+    recipient_emails = [email.strip() for email in recipient_emails]
+    # Check if there is at least one recipient
+    if not recipient_emails:
+        raise Exception("No recipient emails provided.")
+    # Check if all emails are valid
+    for email in recipient_emails:
+        if not is_valid_email(email):
+            raise Exception(f"Invalid email: {email}")
+
+    # Read email subject from a text file and check its length and validity
+    with open('email-info/email_subject.txt', 'r') as file:
+        email_subject = file.read()
+    if len(email_subject) > 78:
+        raise Exception("Subject length exceeds 78 characters.")
+    if not email_subject:
+        raise Exception("Subject is empty.")
+    # Verify that the subject does not contain any line breaks
+    if '\n' in email_subject or '\r' in email_subject:
+        raise Exception("Subject contains line breaks.")
+
+    # Read email message from a text file
+    with open('email-info/email_message.txt', 'r') as file:
+        email_message = file.read()
+    # Check if the message is empty
+    if not email_message:
+        raise Exception("Message is empty.")
+
+    print("Emails ready to be sent.\n")
+    send_emails(sender_email, recipient_emails, email_subject, email_message)
